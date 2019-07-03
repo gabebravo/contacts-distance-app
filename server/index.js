0b1cab6cef2284ef5e4a51828aaa7c294a5a2b27
Gabe Bravo <gabecbravo@gmail.com>
2019-07-03 19:50:35 -0400

    Tried swapping PORT env var

--- server/index.js
+++ server/index.js
@@ -31,6 +31,13 @@ app.get('/api/map-data/:origin/:destination', (req, res) => {
       })
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
